fd1df9b5e66c5e8eb1ddbfdd1533e627cbbc38db
mohamedhh <mohamedhhamza94@gmail.com>
2020-06-21 17:10:01 +0200

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDdSbGU6+KxvbLKW3wwz8YtUaj9TbWzLXicXmPJEXpSlt7erctytoKGXM8xZJ8I/ya9FpDhs0qTXcJMcWAOsb5dqNby9+Yb9W7bzoqmk0KxR0VVZ0LT5pdHaE1eQZYOiKALsOuAD5/HkIyrL7GSE1tFZHu8PX7TM78fk9e1PEIsf0cfirg5sdutUpVCwBIGxJwCn8xY5+/DzdaWVrP/D/v5ZbGwdso5mkp/DnCcZB0kXRzIG83mOHNJwXoPz1IeK86mmkkJlxywgh7cJjEIHmRuiZWRFkwEkf9AXHIegArXivLt6ZE0A6VjM3g5Cn/PziaD3KXE6ZmD5gtiq3/kar8p',
 	}  
 }
